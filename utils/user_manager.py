5a7773fb6f54e8a3d87b753f02bc5c222e115b49
Kalel <148734823+Kalel-StaTeresa@users.noreply.github.com>
2024-05-17 23:43:32 +0800

    Add files via upload

--- utils/user_manager.py
+++ utils/user_manager.py
@@ -4,10 +4,25 @@ from utils.user import User
 class UserManager:
     def __init__(self):
         self.users = []
+        self.data_file = os.path.join('data', 'users.txt')
+        self.load_users()
+
+    def load_users(self):
+        if os.path.exists(self.data_file):
+            with open(self.data_file, 'r') as file:
+                for line in file:
+                    username, password = line.strip().split(',')
+                    self.users.append(User(username, password))
+
+    def save_users(self):
+        with open(self.data_file, 'w') as file:
+            for user in self.users:
+                file.write(f"{user.username},{user.password}\n")
 
     def register(self, user):
         if not any(u.username == user.username for u in self.users):
             self.users.append(user)
+            self.save_users()
             return True
         return False
 
